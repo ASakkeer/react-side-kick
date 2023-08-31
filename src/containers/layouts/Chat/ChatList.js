d15c34da92f84da53d4c1c65e2606d7f534f65ec
sakkeera <sakkeer@mongrov.com>
2023-09-01 00:45:01 +0530

    base chast screen design

--- src/containers/layouts/Chat/ChatList.js
+++ src/containers/layouts/Chat/ChatList.js
@@ -1,15 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatList.css";
 import { ChatRoom } from "./components";
 
 function ChatList() {
+  const [selectedChat, setSelectedChat] = useState(null);
+
+  const chatList = [
+    {
+      id: 1,
+      name: "Barry Allen",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Hey there!",
+      timestamp: "10:30 AM",
+    },
+    {
+      id: 2,
+      name: "Iris West",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Hello!",
+      timestamp: "11:45 AM",
+    },
+    {
+      id: 3,
+      name: "Eobard Thawne",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "How's it going?",
+      timestamp: "1:20 PM",
+    },
+    {
+      id: 4,
+      name: "Cisco Ramon",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Nice weather today!",
+      timestamp: "3:15 PM",
+    },
+    {
+      id: 5,
+      name: "Caitlin Snow",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "See you later!",
+      timestamp: "4:30 PM",
+    },
+    {
+      id: 6,
+      name: "Harrison Wells",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Got any plans for the weekend?",
+      timestamp: "5:45 PM",
+    },
+    {
+      id: 7,
+      name: "Nora West-Allen",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Can you help me with something?",
+      timestamp: "7:10 PM",
+    },
+    {
+      id: 8,
+      name: "Jay Garrick",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "I'll be there in 10 minutes.",
+      timestamp: "8:05 PM",
+    },
+    {
+      id: 9,
+      name: "Patty Spivot",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Thanks for the gift!",
+      timestamp: "9:30 PM",
+    },
+    {
+      id: 10,
+      name: "Wally West",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Did you watch the game?",
+      timestamp: "10:50 PM",
+    },
+    {
+      id: 11,
+      name: "Cecile Horton",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Happy birthday!",
+      timestamp: "11:15 PM",
+    },
+    {
+      id: 12,
+      name: "Ralph Dibny",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "See you at the party!",
+      timestamp: "11:45 PM",
+    },
+    {
+      id: 13,
+      name: "Linda Park",
+      avatarUrl: "https://picsum.photos/800/800",
+      lastMessage: "Let's catch up soon.",
+      timestamp: "12:20 AM",
+    },
+    // Add more characters as needed
+  ];
+
   return (
     <div className="container h-full">
-      <div className="left-container">
-        <p>Hello</p>
+      <div className="left-panel">
+        <div className="header">WhatsApp Web</div>
+        <div className="chat-list">
+          {chatList.map((chat) => (
+            <div
+              className="chat-item"
+              key={chat.id}
+              onClick={() => setSelectedChat(chat)}
+            >
+              <div className="avatar">
+                <img src={chat.avatarUrl} />
+              </div>
+              <div className="chat-info">
+                <div className="chat-name">{chat.name}</div>
+                <div className="chat-message">{chat.lastMessage}</div>
+              </div>
+              <div className="timestamp">{chat.timestamp}</div>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="right-container h-full">
-        <ChatRoom />
+        <ChatRoom selectedChat={selectedChat} />
       </div>
     </div>
   );
--- src/containers/layouts/Chat/components/ChatRoom.js
+++ src/containers/layouts/Chat/components/ChatRoom.js
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
+import "./ChatRoom.css";
 
-const ChatRoom = () => {
+const ChatRoom = (props) => {
+  const { selectedChat } = props;
+  console.log(selectedChat, "khubunhbjb");
   const [inputText, setInputText] = useState("");
   const [messages, setMessages] = useState([
     { text: "Hey, how's it going?", isSender: false },
     { text: "Great! How about you?", isSender: true },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
+    { text: "I am good too.", isSender: false },
     { text: "I am good too.", isSender: false },
     // Add more messages as needed
   ]);
@@ -22,7 +44,10 @@ const ChatRoom = () => {
   };
 
   return (
-    <div style={styles.container}>
+    <div style={styles.container} className="cr-container">
+      <div style={styles.navContainer}>
+        <p>{selectedChat?.name || "--"}</p>
+      </div>
       <div style={styles.messagesContainer}>
         {messages.map((message, index) => (
           <div
@@ -66,14 +91,14 @@ const styles = {
     flexDirection: "column",
     borderLeft: "1px solid #ccc",
     backgroundColor: "#f5f5f5",
-    padding: "20px",
-    overflowY: "scroll",
+    overflowY: "auto",
   },
   messagesContainer: {
     display: "flex",
     flexDirection: "column",
     flex: 1,
     justifyContent: "flex-end",
+    padding: "20px 20px 10px 20px",
   },
   message: {
     display: "flex",
@@ -87,7 +112,21 @@ const styles = {
   },
   inputContainer: {
     display: "flex",
-    marginTop: "20px",
+    // marginTop: "20px",
+    position: "sticky",
+    bottom: "0px",
+    padding: "20px",
+    background: "#fff",
+    boxShadow: "0px -1px 10px 1px #CCCCCC",
+  },
+  navContainer: {
+    display: "flex",
+    // marginTop: "20px",
+    position: "sticky",
+    top: "0px",
+    padding: "10px 20px",
+    background: "#fff",
+    boxShadow: "0px 1px 10px 1px #CCCCCC",
   },
   input: {
     flex: 1,
